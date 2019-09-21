7b08f5042725b8eb25e9434f4fb64f9ada922d49
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-09-21 19:58:32 +0900

    add logging. (incomplete)

--- experiment/urllib3-test.py
+++ experiment/urllib3-test.py
@@ -1,5 +1,25 @@
 import urllib3
 import urllib3.fields
+import logging
+
+rootLogger = logging.getLogger()
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler('root.log')
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+rootLogger.setLevel(logging.DEBUG)
+fh.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+
+rootLogger.addHandler(fh)
+rootLogger.addHandler(ch)
 
 server = "localhost"
 http = urllib3.HTTPConnectionPool(server, maxsize=10)
@@ -7,6 +27,7 @@ http = urllib3.HTTPConnectionPool(server, maxsize=10)
 
 for i in range(11):
 	r = http.request('GET', 'http://' + server + '/', fields={'arg': 'value', 'arg2': '@value'})
+#	r = http.request('GET', 'http://' + server + '/')
 	for name, value in r.__dict__.items():
 	    print(name, type(value))
 	    #print("{0}: {1}".format(name, value))
